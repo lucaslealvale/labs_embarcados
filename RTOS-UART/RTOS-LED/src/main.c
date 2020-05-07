63d0d4c92338588bd161c8a2c8ec9380fdf627cc
Lucas Leal Vale <lucaslealvale01@gmail.com>
2020-05-07 15:48:07 -0300

    lab8c

--- RTOS-UART/RTOS-LED/src/main.c
+++ RTOS-UART/RTOS-LED/src/main.c
@@ -1,9 +1,9 @@
-/*
-* Example RTOS Atmel Studio
-*/
+
 
 #include <asf.h>
 #include "conf_board.h"
+#include <string.h>
+
 #define TASK_LED1_STACK_SIZE (1024/sizeof(portSTACK_TYPE))
 #define TASK_LED1_STACK_PRIORITY (tskIDLE_PRIORITY)
 #define TASK_MONITOR_STACK_SIZE            (2048/sizeof(portSTACK_TYPE))
@@ -11,20 +11,39 @@
 #define TASK_LED_STACK_SIZE                (1024/sizeof(portSTACK_TYPE))
 #define TASK_LED_STACK_PRIORITY            (tskIDLE_PRIORITY)
 
-#define LED1_PIO			PIOA
-#define LED1_PIO_ID			ID_PIOA
-#define LED1_PIO_IDX		0
-#define LED1_PIO_IDX_MASK	(1 << LED1_PIO_IDX)
-
+#define TASK_UARTRX_STACK_SIZE                (1024/sizeof(portSTACK_TYPE))
+#define TASK_UARTRX_STACK_PRIORITY                (tskIDLE_PRIORITY)
+#define TASK_EXECUTE_STACK_SIZE                (1024/sizeof(portSTACK_TYPE))
+#define TASK_EXECUTE_STACK_PRIORITY                (tskIDLE_PRIORITY)
 #define BUT1_PIO            PIOD
 #define BUT1_PIO_ID         16
 #define BUT1_PIO_IDX        28
 #define BUT1_PIO_IDX_MASK   (1u << BUT1_PIO_IDX)
+
+#define LED_PIO PIOC						// periferico que controla o LED
+#define LED_PIO_ID 12						// ID do perif�rico PIOC (controla LED)
+#define LED_PIO_IDX 8						// ID do LED no PIO
+#define LED_PIO_IDX_MASK (1 << LED_PIO_IDX) // Mascara para CONTROLARMOS o LED
+
+#define LED1_PIO PIOA
+#define LED1_PIO_ID ID_PIOA
+#define LED1_PIO_IDX 0
+#define LED1_PIO_IDX_MASK (1 << LED1_PIO_IDX)
+
+#define LED2_PIO PIOC
+#define LED2_PIO_ID ID_PIOC
+#define LED2_PIO_IDX 30
+#define LED2_PIO_IDX_MASK (1 << LED2_PIO_IDX)
+
+#define LED3_PIO PIOB
+#define LED3_PIO_ID ID_PIOB
+#define LED3_PIO_IDX 2
+#define LED3_PIO_IDX_MASK (1 << LED3_PIO_IDX)
 /** Semaforo a ser usado pela task led 
     tem que ser var global! */
 SemaphoreHandle_t xSemaphore;
 QueueHandle_t xQueueChar;
-
+QueueHandle_t xQueueCommand;
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,signed char *pcTaskName);
 extern void vApplicationIdleHook(void);
 extern void vApplicationTickHook(void);
@@ -78,6 +97,16 @@ extern void vApplicationMallocFailedHook(void)
 	/* Force an assert. */
 	configASSERT( ( volatile void * ) NULL );
 }
+
+void giraled(Pio *pio, const pio_type_t ul_type, const uint32_t ul_mask) {
+
+    if (pio_get(pio,ul_type,ul_mask)) pio_clear(pio, ul_mask);
+    else pio_set(pio,ul_mask);
+                
+}
+
+
+
 static void task_led(void *pvParameters) {
   /* We are using the semaphore for synchronisation so we create a binary
   semaphore rather than a mutex.  We must make sure that the interrupt
@@ -100,7 +129,7 @@ static void task_led(void *pvParameters) {
 
   for (;;) {
     if( xSemaphoreTake(xSemaphore, ( TickType_t ) 500) == pdTRUE ){
-      LED_Toggle(LED0);
+      giraled(LED_PIO, LED_PIO_ID, LED_PIO_IDX);
     }
   }
 }
@@ -136,7 +165,6 @@ void LED_init(int estado){
 	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, estado, 0, 0 );
 	
 }
-
 /**
  * \brief This task, when activated, make LED blink at a fixed rate
  */
@@ -150,61 +178,58 @@ static void task_led1(void *pvParameters)
 		vTaskDelay(1500);
 	}
 }
+
 static void task_uartRX(void *pvParameters)
 {
-	xQueueChar = xQueueCreate(10, sizeof(char));
+  char palavra[30];
+  char letra;
+ 
+  for(int i=0;;){
+    if (xQueueReceive( xQueueChar, &letra, ( TickType_t )  100 / portTICK_PERIOD_MS)){
+      if(letra != '\n'){
+        palavra[i]=letra;
+        i++;
+      }else{
+        xQueueSend(xQueueCommand, &palavra,0);
+        i=0;
+      }
+    }
+  }
 }
-void USART1_Handler(char c)
-{
-    //  Dados disponível para leitura
-  if(ret & US_IER_RXRDY){
-      usart_serial_getchar(USART1, &c);
-      printf("%c", c);
+
+static void task_execute(void *pvParameters){
+  char recebmento[30];
+  while(1){
+    if (xQueueReceive( xQueueCommand, &recebmento, ( TickType_t )  100 / portTICK_PERIOD_MS)){
+      if(strcmp(recebmento,"giraLed1")==0 ){
+        giraled(LED1_PIO, LED1_PIO_ID, LED1_PIO_IDX);
+      }else if(strcmp(recebmento,"giraLed2")==0 ){
+        giraled(LED2_PIO, LED2_PIO_ID, LED2_PIO_IDX);
+      }else if(strcmp(recebmento,"giraLed3")==0 ){
+        giraled(LED3_PIO, LED3_PIO_ID, LED3_PIO_IDX);
+      }
+  }
+}}
+void ledInit(void){
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
 }
 
-/**
- * \brief Configure the console UART.
- */
-// static void configure_console(void)
-// {
-// 	const usart_serial_options_t uart_serial_options = {
-// 		.baudrate = CONF_UART_BAUDRATE,
-// #if (defined CONF_UART_CHAR_LENGTH)
-// 		.charlength = CONF_UART_CHAR_LENGTH,
-// #endif
-// 		.paritytype = CONF_UART_PARITY,
-// #if (defined CONF_UART_STOP_BITS)
-// 		.stopbits = CONF_UART_STOP_BITS,
-// #endif
-// 	};
-
-// 	/* Configure console UART. */
-// 	stdio_serial_init(CONF_UART, &uart_serial_options);
-
-// 	/* Specify that stdout should not be buffered. */
-// #if defined(__GNUC__)
-// 	setbuf(stdout, NULL);
-// #else
-// 	/* Already the case in IAR's Normal DLIB default configuration: printf()
-// 	 * emits one character at a time.
-// 	 */
-// #endif
-// }
-
-/**
- *  \brief FreeRTOS Real Time Kernel example entry point.
- *
- *  \return Unused (ANSI-C compatibility).
- */
 static void USART1_init(void){
-  /* Configura USART1 Pinos */
+ 
   sysclk_enable_peripheral_clock(ID_PIOB);
   sysclk_enable_peripheral_clock(ID_PIOA);
   pio_set_peripheral(PIOB, PIO_PERIPH_D, PIO_PB4); // RX
   pio_set_peripheral(PIOA, PIO_PERIPH_A, PIO_PA21); // TX
   MATRIX->CCFG_SYSIO |= CCFG_SYSIO_SYSIO4;
 
-  /* Configura opcoes USART */
+
   const sam_usart_opt_t usart_settings = {
       .baudrate       = 115200,
       .char_length    = US_MR_CHRL_8_BIT,
@@ -232,7 +257,7 @@ static void USART1_init(void){
   NVIC_EnableIRQ(ID_USART1);
 }
 void USART1_Handler(void){
-  uint32_t ret = usart_get_status(USART_COM);
+  uint32_t ret = usart_get_status(USART1);
 
   BaseType_t xHigherPriorityTaskWoken = pdTRUE;
   char c;
@@ -250,6 +275,7 @@ void USART1_Handler(void){
 
   }
 }
+
 uint32_t usart1_puts(uint8_t *pstring){
     uint32_t i ;
 
@@ -259,18 +285,21 @@ uint32_t usart1_puts(uint8_t *pstring){
 }
 int main(void)
 {
+  //considerando a str sendo giraled1,2 e 3 
+  xQueueChar = xQueueCreate(31, sizeof(char));
+  xQueueCommand = xQueueCreate(8, sizeof(char)*31);
 	/* Initialize the SAM system */
 	sysclk_init();
 	board_init();
 	/* Initialize the console uart */
 	USART1_init();
-
+  ledInit();
 	/* Output demo information. */
 	printf("-- Freertos Example --\n\r");
 	printf("-- %s\n\r", BOARD_NAME);
 	printf("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
 
-	xTaskCreate(task_led1, "Led1", TASK_LED1_STACK_SIZE, NULL, TASK_LED1_STACK_PRIORITY, NULL);
+	//xTaskCreate(task_led1, "Led1", TASK_LED1_STACK_SIZE, NULL, TASK_LED1_STACK_PRIORITY, NULL);
 	/* Create task to monitor processor activity */
 	if (xTaskCreate(task_monitor, "Monitor", TASK_MONITOR_STACK_SIZE, NULL,
 			TASK_MONITOR_STACK_PRIORITY, NULL) != pdPASS) {
@@ -282,7 +311,18 @@ int main(void)
 			TASK_LED_STACK_PRIORITY, NULL) != pdPASS) {
 		printf("Failed to create test led task\r\n");
 	}
-
+  if (xTaskCreate(task_led1, "Led1", TASK_LED1_STACK_SIZE, NULL,
+			TASK_LED1_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create test led task\r\n");
+	}
+   if (xTaskCreate(task_uartRX, "task_uartRx", TASK_UARTRX_STACK_SIZE, NULL,
+			TASK_UARTRX_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create test led task\r\n");
+	}
+   if (xTaskCreate(task_execute, "EXECUTE", TASK_EXECUTE_STACK_SIZE, NULL,
+			TASK_EXECUTE_STACK_PRIORITY , NULL) != pdPASS) {
+		printf("Failed to create test led task\r\n");
+	}
 	/* Start the scheduler. */
 	vTaskStartScheduler();
 
